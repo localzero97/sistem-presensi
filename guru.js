c6b45c1db7bd28408b6ded6eb4c1f4eafd9a4cc2
localzero97 <150771231+localzero97@users.noreply.github.com>
2025-08-11 05:11:59 +0800

    Update guru.js

--- guru.js
+++ guru.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzPCSLo5jC7ctfWIkJ1tNVjJY34N-0_XZQdmhNyt8LUmPLm4upT40ilaJ7qTLJXzrACcQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwpKj9wtzqpAavQ96gM7Dgx0sfrsqnsaEmqKS3BdFxLZyLz3z9fEB9bJ_gTVgptMERFuQ/exec";
 const adminLoginContainer = document.getElementById('admin-login-container');
 const adminLoginForm = document.getElementById('admin-login-form');
 const adminLoginMessage = document.getElementById('admin-login-message');
